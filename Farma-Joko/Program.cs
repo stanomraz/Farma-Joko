c22560086622355e8a9f4b2e3bbe776f913dd447
Belaxin <tomasbelaxinen@gmail.com>
2025-04-13 15:58:56 +0200

    -push test

--- Farma-Joko/Program.cs
+++ Farma-Joko/Program.cs
@@ -8,6 +8,7 @@ namespace Farma_Joko
         [STAThread]
         static void Main()
         {
+            // Pipik
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
